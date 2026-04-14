abadc2e3244798281be443ec53c6452776f2f8cd
welkwolf <maxfidrya22@gmail.com>
2026-04-14 11:57:11 +0300

    Update testMiniApp.js

--- testMiniApp.js
+++ testMiniApp.js
@@ -1,4 +1,3 @@
 createText('Нижняя кнопка перекидывает на Гугл');
 createButton('Кнопочка', 'https://google.com');
 
-        const savedUser = localStorage.getItem('volgura_user') || sessionStorage.getItem('volgura_user');
